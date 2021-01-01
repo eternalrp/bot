e36a249d00d2ea558760d34dfb09f709a4c70999
eternalrp <76838903+eternalrp@users.noreply.github.com>
2021-01-01 15:06:48 +0300

    Add files via upload

---
+++ events/ready.js
@@ -0,0 +1,16 @@
+const chalk = require('chalk');
+const moment = require('moment');
+const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
+
+var prefix = ayarlar.prefix;
+
+module.exports = client => {
+  console.log(`[BOT] | Botun destekçileri ve komutları başarıyla yüklendi, işlemler tamamlandı !`);
+  console.log(`[BOT] | (${client.user.username}) Eternal Moderation
+  ‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒`);
+  client.user.setStatus("online");
+  client.user.setActivity("Eternal 💚 RolePlay", { type: "PLAYING"}); //// TYPE - WATCHING , PLAYING , LISTENING gibi değiştirilebilir.
+  console.log(`Eternal Moderation Hizmetinizde.`);
+
+};
